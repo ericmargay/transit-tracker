c515dc60ead6585c437062657f18616582fe95ed
Eric Margay <ericmargay@gmail.com>
2026-02-23 18:01:33 -0600

    first changes

---
+++ iOS/TransitTracker/Features/Map/MapView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TransitTracker<
+//
+//  Created by Melo on 22/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS/TransitTracker/TransitTracker/TransitTracker_App.swift
@@ -0,0 +1,17 @@
+//
+//  TransitTracker_App.swift
+//  TransitTracker<
+//
+//  Created by Melo on 22/02/26.
+//
+
+import SwiftUI
+
+@main
+struct TransitTracker_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
